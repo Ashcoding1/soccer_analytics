ae75b7c8ad1c635445b22a6804665f40be9f6bfa
Derek M Berger <dmberger.dev@gmail.com>
2022-06-01 15:02:37 -0400

    get testing working with Jest

---
+++ frontend/tests/test.test.ts
@@ -0,0 +1,8 @@
+import { shuffle } from "lodash";
+// import { test, expect, describe } from "@jest/globals";
+
+describe("testing setup", () => {
+    test("can't fail", () => {
+        expect(true).toBe(true);
+    });
+});
