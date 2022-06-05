6c114ff6e52001fdf050e55ac5c97a1f8a3f8087
Derek M Berger <dmberger.dev@gmail.com>
2022-06-05 13:57:19 -0400

    ensure column names are snake_case

--- fix.sql
+++ fix.sql
@@ -82,3 +82,53 @@ ALTER TABLE Match RENAME TO matches;
 ALTER TABLE Player RENAME TO players;
 ALTER TABLE Team RENAME TO teams;
 PRAGMA foreign_key_check;
+
+CREATE TABLE IF NOT EXISTS "team_attrs" (
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
+    team_api_id INTEGER,
+    date    TEXT,
+    buildUpPlaySpeed    INTEGER,
+    buildUpPlaySpeedClass    TEXT,
+    buildUpPlayDribbling    INTEGER,
+    buildUpPlayDribblingClass    TEXT,
+    buildUpPlayPassing    INTEGER,
+    buildUpPlayPassingClass    TEXT,
+    buildUpPlayPositioningClass    TEXT,
+    chanceCreationPassing    INTEGER,
+    chanceCreationPassingClass    TEXT,
+    chanceCreationCrossing    INTEGER,
+    chanceCreationCrossingClass    TEXT,
+    chanceCreationShooting    INTEGER,
+    chanceCreationShootingClass    TEXT,
+    chanceCreationPositioningClass    TEXT,
+    defencePressure    INTEGER,
+    defencePressureClass    TEXT,
+    defenceAggression    INTEGER,
+    defenceAggressionClass    TEXT,
+    defenceTeamWidth    INTEGER,
+    defenceTeamWidthClass    TEXT,
+    defenceDefenderLineClass    TEXT,
+    FOREIGN KEY (team_api_id) REFERENCES Team_Attributes(team_api_id)
+);
+
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlaySpeed TO buildup_play_speed;
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlaySpeedClass TO buildup_play_speed_class;
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlayDribbling TO buildup_play_dribbling;
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlayDribblingClass TO buildup_play_dribbling_class;
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlayPassing TO buildup_play_passing;
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlayPassingClass TO buildup_play_passing_class;
+ALTER TABLE team_attrs RENAME COLUMN buildUpPlayPositioningClass TO buildup_play_positioning_class;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationPassing TO chance_creation_passing;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationPassingClass TO chance_creation_passing_class;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationCrossing TO chance_creation_crossing;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationCrossingClass TO chance_creation_crossing_class;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationShooting TO chance_creation_shooting;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationShootingClass TO chance_creation_shooting_class;
+ALTER TABLE team_attrs RENAME COLUMN chanceCreationPositioningClass TO chance_creation_positioning_class;
+ALTER TABLE team_attrs RENAME COLUMN defencePressure TO defence_pressure;
+ALTER TABLE team_attrs RENAME COLUMN defencePressureClass TO defence_pressure_class;
+ALTER TABLE team_attrs RENAME COLUMN defenceAggression TO defence_aggression;
+ALTER TABLE team_attrs RENAME COLUMN defenceAggressionClass TO defence_aggression_class;
+ALTER TABLE team_attrs RENAME COLUMN defenceTeamWidth TO defence_team_width;
+ALTER TABLE team_attrs RENAME COLUMN defenceTeamWidthClass TO defence_team_width_class;
+ALTER TABLE team_attrs RENAME COLUMN defenceDefenderLineClass TO defence_defender_line_class;
