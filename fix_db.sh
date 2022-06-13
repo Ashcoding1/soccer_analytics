8c365c6878a1fe1b3c8b37388fae4b9f378d4c37
Derek M Berger <dmberger.dev@gmail.com>
2022-06-13 13:55:09 -0400

    finalize DB fixing

--- fix_db.sh
+++ fix_db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 rm -rvf broken_db && unzip -o archive.zip -d broken_db
 echo "Unzipped archive downloaded from https://www.kaggle.com/datasets/hugomathien/soccer"
-echo "Removing broken foreign key constraints from Team_Attributes and"
-echo "Player_Attributes tables and renaming tables to plurals, snake_case..."
-sqlite3 broken_db/database.sqlite < fix.sql && \
+echo "Removing broken foreign key constraints and making names consistent"
+sqlite3 broken_db/database.sqlite -echo < fix.sql && \
 echo -n "Successfully fixed up db. Copying to backend/db.sqlite3..."
 cp broken_db/database.sqlite backend/db.sqlite3
 echo "done."
