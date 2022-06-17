78c54579be89901f080a4c43d452c19c445bd7fc
Derek M Berger <dmberger.dev@gmail.com>
2022-06-17 16:19:37 -0400

    ensure install script exits on error

--- install_dev.sh
+++ install_dev.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e  # exit on error
 ### Python Setup. Assumes pyenv is installed with python 3.10.4 compiled and available
 
 # Ensure we use correct python version for venv creation
+if ! command -v pyenv &> /dev/null
+then
+    echo "\`pyenv\` could not be found"
+    exit 1
+fi
+
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
@@ -16,6 +23,11 @@ echo "Installing dev dependencies to virtual environment..."
 # NOTE: see https://stackoverflow.com/a/20720019 for the approach below
 python -m pip install -r install/requirements/dev.txt
 
+if ! command -v nvm &> /dev/null
+then
+    echo "\`nvm\` could not be found"
+    exit 1
+fi
 # node setup. Assumes nvm (Node version manager) is installed and available on your PATH
 nvm install v16.15.0 --lts
 nvm use v16.15.0
